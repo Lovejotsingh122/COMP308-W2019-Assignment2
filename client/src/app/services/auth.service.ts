03eb9b0da152c54a3bc7bc561a5c35fe2c393699
lovejotsingh122 <lovejotsangha122@gmail.com>
2019-04-06 20:53:34 -0400

    updated heroku link

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
   user: User;
   private authToken: any;
 
-  private endpoint = 'https://comp308-w2019-assignment2-love.herokuapp.com/api/';
+  private endpoint = 'https://comp308-w2019-assignment2-love.herokuapp.com/api/contact=list';
   //private endpoint = 'http://localhost:3000/api/';
 
   private httpOptions = {
